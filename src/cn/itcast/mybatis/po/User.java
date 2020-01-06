f41f87c146b2cf79a6afe1078795491550b4ac53
Dong Bo Yuan <823883175@qq.com>
2020-01-06 22:44:30 +0800

    add lombok

--- src/cn/itcast/mybatis/po/User.java
+++ src/cn/itcast/mybatis/po/User.java
@@ -2,58 +2,31 @@ package cn.itcast.mybatis.po;
 
 import java.util.Date;
 
+import lombok.Data;
+
 /**
  * 
- * <p>Title: User</p>
- * <p>Description:用户po </p>
- * <p>Company: www.itcast.com</p> 
- * @author	传智.燕青
- * @date	2015-4-22上午10:24:16
- * @version 1.0
+ * @ClassName: User
+ * @Description: 用户po
+ *
+ * @version: v1.0.0
+ * @author dongby1
+ * @date 2020/01/06 22:43:43
  */
+@Data
 public class User {
-	
-	//属性名和数据库表的字段对应
+
+	// 属性名和数据库表的字段对应
 	private int id;
 	private String username;// 用户姓名
 	private String sex;// 性别
 	private Date birthday;// 生日
 	private String address;// 地址
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public Date getBirthday() {
-		return birthday;
-	}
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", sex=" + sex
-				+ ", birthday=" + birthday + ", address=" + address + "]";
+		return "User [id=" + id + ", username=" + username + ", sex=" + sex + ", birthday=" + birthday + ", address="
+				+ address + "]";
 	}
-
 
 }
